46c0c89c8a89616c1cce0374f7126f018c7ca3da
Mohamed Talaat <mohamedtalaat333@gmail.com>
2023-09-20 18:00:56 +0300

    latest

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,6 +1,6 @@
 // import { useEffect, useState } from 'react';
-import { useLoaderData } from "react-router-dom";
-import EventsList from 'components/EventsList';
+import { json, useLoaderData } from "react-router-dom";
+import EventsList from "components/EventsList";
 
 function EventsPage() {
   const eventsList = useLoaderData();
@@ -9,7 +9,7 @@ function EventsPage() {
 
   return (
     <>
-      <div style={{ textAlign: 'center' }}>
+      <div style={{ textAlign: "center" }}>
         {/* {isLoading && <p>Loading...</p>}
         {error && <p>{error}</p>} */}
       </div>
@@ -27,7 +27,7 @@ export async function loader() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    // setError("Fetching events failed.");
+    return json({ message: "Couldn't fetch events." }, { status: 500 });
   } else {
     const resData = await response.json();
     return resData.events;
