08aed4515b721312f936226dd8c0dd1b6e1c1a9a
Mohamed Talaat <mohamedtalaat333@gmail.com>
2023-09-24 17:52:19 +0300

    latest

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,7 +2,7 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import EventDetailsPage, { loader as eventDetailsLoader } from "pages/EventDetails";
 import EventsPage, { loader as eventsLoader } from "pages/Events";
 import HomePage from "pages/Home";
-import NewEventPage from "pages/NewEvent";
+import NewEventPage, { action as newEventAction } from "pages/NewEvent";
 import EditEventPage from "pages/EditEvent";
 import RootLayout from "pages/Root";
 import EventsRootLayout from "pages/EventsRoot";
@@ -33,7 +33,7 @@ const router = createBrowserRouter([
               { path: "edit", element: <EditEventPage /> },
             ],
           },
-          { path: "new", element: <NewEventPage /> },
+          { path: "new", element: <NewEventPage />, action: newEventAction },
         ],
       },
     ],
--- frontend/src/pages/NewEvent.js
+++ frontend/src/pages/NewEvent.js
@@ -1,7 +1,10 @@
+import EventForm from "components/EventForm";
+import { json } from "react-router-dom";
+
 function NewEventPage() {
     return (
         <>
-            <h1>This is from New Event Page</h1>
+            <EventForm />
 
             <span>Yataaa</span>
         </>
@@ -9,3 +12,29 @@ function NewEventPage() {
 }
 
 export default NewEventPage;
+
+export async function action({ request, params }) {
+    const data = await request.formData();
+
+
+    const eventData = {
+        title: data.get("title"),
+        image: data.get("image"),
+        date: data.get("date"),
+        description: data.get("description"),
+    }
+
+    console.log(eventData);
+
+    const response = await fetch("http://localhost:8080/events", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(eventData)
+    })
+
+    if (!response.ok) {
+        throw json({ message: "Could not save event." }, { status: 500 });
+    }
+}
